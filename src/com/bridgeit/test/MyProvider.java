04868a772d0862c5ce23c8e9b338d855e6219860
iNeha <snehgaik11@gmail.com>
2019-07-03 16:23:59 +0530

    new

---
+++ src/com/bridgeit/test/MyProvider.java
@@ -0,0 +1,8 @@
+package com.bridgeit.test;
+
+public interface MyProvider{
+  String userName="root";
+  String password="admIn@111";
+  String ConnectionUrl="jdbc:mysql://localhost:3306/LoginRegistration1";
+  
+}
